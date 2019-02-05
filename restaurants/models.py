2dce132efdcec87a8001b3ae25783f4dda0a6371
Thuraya AlAteeqi <ThurayaAlAteeqi@Thurayas-MacBook-Air.local>
2019-02-05 15:43:03 +0300

    submission

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+	name = models.CharField(max_length=30)
+	description = models.CharField(max_length=200)
+	opening_time = models.TimeField()
+	closing_time = models.TimeField()
